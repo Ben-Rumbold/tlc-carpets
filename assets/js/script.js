ba40cde487ca61dba476d5d807cef83b76ca1746
Ben-Rumbold <b.rumbold19@gmail.com>
2024-01-27 01:24:32 +0000

    completed contact page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+function sendEmail() {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "username",
+    Password: "password",
+    To: "them@website.com",
+    From: "you@isp.com",
+    Subject: "This is the subject",
+    Body: "And this is the body",
+  }).then((message) => alert(message));
+}
+
+// all the other shit (awful code, sorry mum)
 document.addEventListener("DOMContentLoaded", function () {});
 
 console.log("Hi!");
